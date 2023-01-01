c62bf253179e86902276c47617d899aced512279
John Glen Falceso <johnglenfalceso@protonmail.com>
2023-01-01 17:56:09 +0800

    added README.md and modified metadata.py

---
+++ README.md
@@ -0,0 +1,13 @@
+# CurrenciesDenominationAPI
+The conversion between two currencies and the nearest denomination of the converted currency. Accepted currencies at the moment are PHP, USD, EUR, AUD, JPY. 
+
+## Accepted Currencies 
+[PHP, USD, EUR, AUD, JPY]
+
+## Example Usage
+* https://currency-denomination-api.onrender.com/denomination?currency_from=PHP&currency_to=USD&from_value=315.75
+* returns ```{"from":"PHP","to":"USD","exchange_rate":0.017969,"date":"2023-01-01","from_value":315.75,"to_value":5.67,"to_denomination":{"5":1,"0.50":1,"0.10":1,"0.05":1,"0.01":2}}```
+
+## Docs
+check out our documentation here:
+https://currency-denomination-api.onrender.com/docs
--- metadata.py
+++ metadata.py
@@ -10,10 +10,13 @@ description = """The conversion between two currencies and the nearest denominat
 [PHP, USD, EUR, AUD, JPY]
 
 ## Example Usage
-* https.../denomination?currency_from=PHP&currency_to=USD&from_value=315.75
+* https://currency-denomination-api.onrender.com/denomination?currency_from=PHP&currency_to=USD&from_value=315.75
 
-### returns
-* {"from":"PHP","to":"USD","exchange_rate":0.017969,"date":"2023-01-01","from_value":315.75,"to_value":5.67,"to_denomination":{"5":1,"0.50":1,"0.10":1,"0.05":1,"0.01":2}}
+* returns ```{"from":"PHP","to":"USD","exchange_rate":0.017969,"date":"2023-01-01","from_value":315.75,"to_value":5.67,"to_denomination":{"5":1,"0.50":1,"0.10":1,"0.05":1,"0.01":2}}```
+
+## Docs
+check out our documentation here
+https://currency-denomination-api.onrender.com/docs
 """
 version="1.0.pogi"
 
